29a6e3101b9e60ab5c26b5bc2f075803c0a4df56
Miguel Herrera <mherr059@fiu.edu>
2015-07-26 18:41:21 -0400

    Created Run Analysis Script Template

---
+++ run_analysis.r
@@ -0,0 +1,44 @@
+# Title: 		Getting and Cleaning - Course Project
+# Description: 	The purpose of this project is to demonstrate your ability to collect, work with, and 
+#				clean a data set.
+# Author: 		Miguel Herrera
+
+###############################################
+#### Course Project Tasks - Run Analysis   ####
+###############################################
+# 0. Setup process
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each 		variable for each activity and each subject.
+
+
+###############################################
+#### Part 0 - Setup Process ####
+###############################################
+
+
+###############################################
+#### Part 1 - Merging train/test data sets ####
+###############################################
+
+
+#######################################################
+#### Part 2 - Extract mean/sd for each measurement ####
+#######################################################
+
+
+#################################################
+#### Part 3 - Name activites in the data set ####
+#################################################
+
+
+##################################################
+#### Part 4 - Label data set w/ vriable names ####
+##################################################
+
+
+#############################################
+#### Part 5 - Create final tidy data set ####
+#############################################
